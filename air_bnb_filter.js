04e6b08a8a80a6364a53403d2afe02156c262a22
Mirek Kukla <mirekkukla@gmail.com>
2018-11-27 22:01:43 +0100

    works with jquery + hiding

--- air_bnb_filter.js
+++ air_bnb_filter.js
@@ -2,12 +2,19 @@
 
 // https://www.airbnb.com/s/Prague--Czech-Republic/homes?refinement_paths%5B%5D=%2Fhomes&checkin=2018-11-30&checkout=2018-12-04&adults=2&price_min=23&price_max=52&query=Prague%2C%20Czech%20Republic&place_id=ChIJi3lwCZyTC0cRkEAWZg-vAAQ&allow_override%5B%5D=&s_tag=duuZACVT
 
+// include jqury
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
 //$$("._i655ore")[2].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
 function getOuterDiv(totalAmountNode) {
     var node = totalAmountNode;
-    while (node.parentNode.className != "_fhph4u") {
+    while (node.parentNode.className !== "_fhph4u") { // This hasn't been changing... yet
         node = node.parentNode;
-        if (node == null) {
+        console.log(node);
+        if (node === null) {
             console.log("Couldn't find parent for");
             console.log(totalAmountNode);
             return null;
@@ -17,39 +24,50 @@ function getOuterDiv(totalAmountNode) {
     return node;
 }
 
-
 var maxTotalAllowed = 180;
+var CLASS_OF_DIV_WITH_TOTAL = "._17oldnte";
+var candidateLeafNodes = $(CLASS_OF_DIV_WITH_TOTAL); // TODO: this gets changed... each relelase??
 
-var candidateLeafNodes = $$("._i655ore");
-for (var i = 0; i < candidateLeafNodes.length; i++) {
-    var candidateLeaf = candidateLeafNodes[i];
+candidateLeafNodes.each(function(i, candidateLeaf) {
+
     if (candidateLeaf.children.length === 0) {
-        continue;
+        return;
     }
 
     var text = candidateLeaf.textContent;
-    if (text[0] !== "$") {
-        continue;
+    if (!text.includes("$")) {
+        return;
     }
 
-    // at this point the string is e.g. "$251total"
-    var amountStr = candidateLeaf.textContent.split('t')[0];
-    var amountInt = amountStr.slice(1, amountStr.length);
 
-    if (amountInt > maxTotalAllowed) {
+    // The leaf string used to be e.g. "$251total" or "Price$265" - for some reason it changes??
+    var inner_digits_re = /[^0-9]*([0-9]+)[^0-9]*/;
+    var re_result = text.match(inner_digits_re);
+    if (re_result === null || re_result.length !== 2) {
+        console.log("Something went wrong with regex, searched '" + text + "', got " + re_result);
+        return;
+    }
+    var amountStr = re_result[1];
+
+    // todo: str to int compariosn??
+    if (amountStr > maxTotalAllowed) {
         console.log("i is " + i + " amountStr "  + amountStr);
-        var totalAmountNode = candidateLeafNodes[i];
 
-        var outerDivNode = getOuterDiv(totalAmountNode);
-        if (outerDivNode == null) {
+        console.dir(candidateLeaf);
+        var outerDivNode = getOuterDiv(candidateLeaf);
+        if (outerDivNode === null) {
             console.log("Didn't find outer div, quitting");
-            break;
+            return;
         }
 
         console.log("About to remove:");
         console.log(outerDivNode); // console.dir shows it as a javascript object (if you want to see all properties etc)
-        console.log("Amount is " + amountInt);
-        outerDivNode.remove();
+        console.log("Amount is " + amountStr);
+        // $(outerDivNode).find("*").hide();
+        // $(outerDivNode).hide();
+
+        $(outerDivNode).attr("style", "display: none !important");
+        $(outerDivNode).find("*").attr("style", "display: none !important");
     }
-}
+});
 
