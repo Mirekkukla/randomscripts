a73eb47ed2931d767b391282d4fab45d73dd6f23
Mirek Kukla <mirekkukla@gmail.com>
2018-12-28 15:01:22 -0800

    documentation for google flights script

--- flight_results_tab.py
+++ flight_results_tab.py
@@ -2,11 +2,23 @@
 import webbrowser
 import argparse
 
+
+"""
+Script to help you find the cheapest day to fly between two sets of cities.
+Provide "from airport(s)", "to airport(s)", and "departure date" and we'll open a new
+browser tab with google flights results for the given parameters.
+
+Expected use: run multiple times with parameters passed in as command-line arguments,
+where you simply change the "departure date" parameter in between executions.
+
+Example:
+python flight_results_tab.py -f "PRG,VIE" -t "SFO,SJC" -d "2018-09-13"
+"""
 def main():
     # pull default airports from command line
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', action='store', help='from airport(s), ex: "PRG,VIE"')
-    parser.add_argument('-t', action='store', help='to airport(s), ex: "PRG,VIE"')
+    parser.add_argument('-t', action='store', help='to airport(s), ex: "SFO,SJC"')
     parser.add_argument('-d', action='store', help='departure_date, ex: "2018-09-13"')
     args = parser.parse_args()
     
@@ -41,11 +53,7 @@ def main():
     full_url = base_url + from_airports + '.' + to_airports + '.' + departure_date + all_static_params
     # EX: https://www.google.com/flights#flt=PRG,VIE.JRO.2018-09-28;c:USD;e:1;sd:1;t:f;tt:o
 
-
     webbrowser.open(full_url)
-    # webbrowser.open(url[, new=0[, autoraise=True]])
-
-    # print script call with parameters that would have done the current query
 
 if __name__ == "__main__":
     main()
