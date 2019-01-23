953756879d709bbfdd6d762919cc3aa0f013ee0b
Mirek Kukla <mirekkukla@gmail.com>
2019-01-23 10:08:09 +0100

    cleanup up portal code, add checkbox logic

--- populate_portal.js
+++ populate_portal.js
@@ -1,32 +1,34 @@
+var NAME = "Bobdole";
+var EMAIL = "BobDole123@gmail.com";
+
 /**
  * Populate all input boxes that look name-related with a fake name, and similarly
- * with a fake email address.
+ * with a fake email address. Check checkboxes indicate the acceptance of terms.
  * 
  * We consider an input box "name-related" if it has an attribute value
  * containing the case-insensitive substring "name" (and likewise with "email").
  */
-
-var NAME = "Bobdole";
-var EMAIL = "BobDole123@gmail.com";
-
 $("input").each(function(i, node) {
-    var attNames = node.getAttributeNames();
-    for (var j = 0; j < attNames.length; j++) {
- 
-        var attrName = attNames[j];
+
+    for (var attrName of node.getAttributeNames()) {
         var attrVal = node.getAttribute(attrName);
 
-        var nameRegex = /[Nn][Aa][Mm][Ee]/
-        if (nameRegex.test(attrVal)) {
+        if (node.type === "checkbox" && /accept/i.test(attrVal)) {
+            console.log("Checkbox match on attr=" + attrName + " value=" + attrVal); 
+            node.checked = true;
+            console.log(node);
+            return;
+        }
+
+        if (/name/i.test(attrVal)) {
             console.log("Name match on attr=" + attrName + " value=" + attrVal);
             node.value = NAME;
             console.log(node);
             return;
         }
 
-        var emailRegex = /[Ee][Mm][Aa][Ii][Ll]/;
-        if (emailRegex.test(attrVal)) {
-            console.log("Name match on attr=" + attrName + " value=" + attrVal);
+        if (/mail/i.test(attrVal)) {
+            console.log("Email match on attr=" + attrName + " value=" + attrVal);
             node.value = EMAIL;
             console.log(node);
             return;
