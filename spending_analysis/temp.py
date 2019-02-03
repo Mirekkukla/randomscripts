709016035113777b7e485098868420d7903f82cc
Mirek Kukla <mirekkukla@gmail.com>
2019-02-03 07:33:02 +0100

    temp

--- spending_analysis/temp.py
+++ spending_analysis/temp.py
@@ -11,8 +11,10 @@ raw_data_folder_path = os.path.join(utils.get_base_folder_path(), "raw_data")
 
 
 
-with open(os.path.abspath("/Users/mirek/old_chase_extract_credit_data/uncategorized_lines.tsv")) as f:
+with open(os.path.abspath("/Users/mirek/old_chase_extract_credit_data/uncategorized_lines.tsv")) as f1:
+    uncategorized_lines = f.read().splitlines()
+
+with open(os.path.abspath("/Users/mirek/chase_extract_credit_data/manually_categorized_tx.tsv")) as f2:
     old_categorized = f.read().splitlines()
 
-with open(os.path.abspath("/Users/mirek/chase_extract_credit_data/manually_categorized_tx.tsv")) as f:
-
+with
