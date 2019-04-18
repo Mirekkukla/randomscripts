ebf4adbcbeda3c2c14d9f17d244e88302a0feaca
Mirek Kukla <mirekkukla@gmail.com>
2019-04-18 12:52:35 +0800

    make it plugin friendly

--- chrome-snippets/typingPercentage.js
+++ chrome-snippets/typingPercentage.js
@@ -1,19 +1,24 @@
 /**
- * Log the % of letters you got right on a typing study drill
- * e.g. https://www.typingstudy.com/lesson/3/extra_key_drill
- */
+* Log the % of letters you got right on a typing study drill
+* e.g. https://www.typingstudy.com/lesson/3/extra_key_drill
+*/
 (function() {
     "use strict";
+    console.log("Scoring typing results");
+
+    // the Shortkey pluging doesn't have the chrome "$" selector bindings
+    let $$ = document.querySelectorAll.bind(document);
+
     let totalCorrect = 0;
     let total = 0;
-
     for (let cellDiv of $$(".graph .result")) {
         let fractionStr = cellDiv.innerText;
         let fractionArr = fractionStr.split("/");
         totalCorrect += parseInt(fractionArr[0]);
         total += parseInt(fractionArr[1]);
     }
-
     let percent = totalCorrect * 100.0 / total;
-    console.log(`Total: ${totalCorrect}/${total} = ${percent}%`);
+    let msg = `Total: ${totalCorrect}/${total} = ${percent}%`;
+    console.log(msg);
+    alert(msg);
 })();
