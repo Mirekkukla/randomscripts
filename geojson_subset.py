acd039a6a6e5c8f52d5e464280da783fe9d068b1
Mirek Kukla <mirekkukla@gmail.com>
2021-02-17 11:07:48 -0800

    Book review density

--- geojson_subset.py
+++ geojson_subset.py
@@ -1,10 +1,12 @@
 n = 0
-with open("/Users/mirek/Desktop/canada-forward-sortation-areas.geojson", "r") as f_read, open("/Users/mirek/Desktop/canada_ontario.geojson", "w") as f_write:
+with open("/Users/mirek/Desktop/canada_fsa_2016_census_001.pretty.geojson", "r") as f_read, open("/Users/mirek/Desktop/canada_fsa_2016_census_001_province_35_.pretty.geojson", "w") as f_write:
     for line in f_read:
 
         if "PRUID" in line:
             if "\"PRUID\": \"35\"" in line:
                 n += 1
+                print line
+                exit(0)
                 f_write.write(line)
             continue
 
